9faa8f3d539941847db459b211af397e2de293c6
mrdennis1212 <dennis.lueke@proton.me>
2023-01-26 09:37:10 +0100

    Added started logs on register

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const colors = require('colors')
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const consolePrefix = `${'['.blue}${'dbd-soft-ui'.yellow}${']'.blue} `
 
-module.exports.register = async function (client) {
+module.exports.register = async function (client, data) {
     if (client === undefined) return console.log(`${consolePrefix} ${'Client has not been specified!'.red}`)
+    if (data === undefined || data.hasOwnProperty("key") === false || data.hasOwnProperty("dashboard_url") === false) return console.log(consolePrefix + "The endpoint information has not been entered!");
 
     console.log(consolePrefix + "Log collection is " + colors.brightGreen("ACTIVE"))
+
+    let initial = {
+        title: "Bot Started",
+        description: "Bot has been started!",
+    }
+
+    initial.description = `[${new Date().toLocaleString()}] ${initial.description}`
+
+    await fetch(data.dashboard_url + "/stats/logs/update", {
+        method: "post",
+        body: JSON.stringify(initial),
+        headers: { "Content-Type": "application/json", "Authorization": "Bearer " + data.key }
+    }).catch(err => console.log(err))
 }
 
 module.exports.send = async function (client, data) {
@@ -16,23 +30,17 @@ module.exports.send = async function (client, data) {
     data.description = `[${new Date().toLocaleString()}] ${data.description}`
 
     async function submitStats(data) {
-        console.log(consolePrefix + "Sending log to the dashboard..." + data.title + " " + data.description + " " + data.dashboard_url + " " + data.key)
 
         let list = {
             title: data.title,
             description: data.description,
         }
 
-        console.log(consolePrefix + "Log to be sent to the dashboard: " + data.dashboard_url + "/stats/logs/update" + " : " + list.title + " : " + list.description);
-
         await fetch(data.dashboard_url + "/stats/logs/update", {
             method: "post",
             body: JSON.stringify(list),
             headers: { "Content-Type": "application/json", "Authorization": "Bearer " + data.key }
         }).catch(err => console.log(err))
-
-        console.log(consolePrefix + "Log has been sent to the dashboard!")
     }
-
     submitStats(data)
 }
